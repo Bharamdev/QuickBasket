c4fe2903d8bbe18c27cde9ee6df2c5dfdedc05c1
Bharamdev4 <bharamdevbansal2132004@gmail.com>
2025-08-06 14:47:03 +0530

    Re-added project files after cleaning .gitignore

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDb
+connectDB();
+
+app.get("/",(req,res) => {
+    res.send("Welcom to the Quick Basket!");
+});
+
+// API routes
+app.use("/api/users",userRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server is runnig on http://localhost:${PORT}`);
+    
+})
