359ebc5f119f5d6c11e722e4c9e11fd75244a810
Bharamdev4 <bharamdevbansal2132004@gmail.com>
2025-09-26 13:57:01 +0530

    Update

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get("/my-orders", protect, async(req,res) => {
     try {
         // Find orders for the authenticated user
-        const orders = await Order.find({ user: req.user._id }).sort({ createdAt: -1}); // sort ny most recent orders
+        const orders = await Order.find({ user: req.user._id }).sort({ createdAt: -1}); // sort by most recent orders
         res.json(orders);
     } catch (error) {
         console.error(error);
